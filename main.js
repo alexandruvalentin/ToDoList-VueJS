529a0cec9999868a9d6718b3d85a79c89a62bef4
Alexandru Grapa <alexandru22valentin@gmail.com>
2022-01-08 12:21:49 +0000

    Add data, computed and methods to the app

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-const app = Vue.createApp ({
+const app = Vue.createApp({
     data() {
         return {
-
+            tasks: []
+        }
+    },
+    computed: {
+        taskCount() {
+            return this.tasks.length;
+        }
+    },
+    methods: {
+        addNewTask(newTask) {
+            this.tasks.push(newTask);
+        },
+        removeTask(task) {
+            this.tasks.splice(this.tasks.indexOf(task), 1);
         }
     }
 })
